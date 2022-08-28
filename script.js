1dd84ba3180dfbe8ce2f4c767541bd5368d7cced
PRAVEENTHANGAMUTHU <99811900+PRAVEENTHANGAMUTHU@users.noreply.github.com>
2022-08-28 10:00:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ console.log(Math.trunc(trc)); /* Output: 2 */
 /*  Min & Max (-Infinity & Infinity) */
 let mn = Math.min();
 console.log(mn); /* Output: infinity */
-let mx = Math.min();
+let mx = Math.max();
 console.log(mx); /* Output: -infinity */
 let mnv = Math.min(-5, 1, 5);
 console.log(mnv); /* Output: -5 */
@@ -169,28 +169,28 @@ let cob = 7;
 let coc = "5";
 let cod = "six";
 /* Equal to */
-console.log(coa==cob);  /* Output: false */
-console.log(coa==coc);  /* Output: true */
+console.log(coa == cob); /* Output: false */
+console.log(coa == coc); /* Output: true */
 
 /* If we use three operators like this "===" or "!==", It will check the data types also. It is called as Strict Comparison. It is also comes from Type coeration.*/
-console.log(coa===coc);  /* Output: false */
+console.log(coa === coc); /* Output: false */
 
 /* Not Equal to */
-console.log(coa!=cob);  /* Output: true */
-console.log(coa!=coc);  /* Output: false */
-console.log(coa!==coc);  /* Output: true */
+console.log(coa != cob); /* Output: true */
+console.log(coa != coc); /* Output: false */
+console.log(coa !== coc); /* Output: true */
 
 /* Greater than */
-console.log(coa>cob);  /* Output: false */
+console.log(coa > cob); /* Output: false */
 
 /* Lesser than */
-console.log(coa<cob);  /* Output: True */
+console.log(coa < cob); /* Output: True */
 
 /* Greater than or Equal to */
-console.log(coa>cob);  /* Output: false */
+console.log(coa > cob); /* Output: false */
 
 /* Lesser than or Equal to*/
-console.log(coa<cob);  /* Output: True */
+console.log(coa < cob); /* Output: True */
 
 /* Arithmetic Operators */
 
@@ -198,43 +198,46 @@ let aoa = 5;
 let aob = 4;
 
 /* Addition */
-console.log(aoa+aob);  /* Output: 9 */
+console.log(aoa + aob); /* Output: 9 */
 /* Substraction */
-console.log(aoa-aob);  /* Output: 1 */
+console.log(aoa - aob); /* Output: 1 */
 /* Multiplication */
-console.log(aoa*aob);  /* Output: 20 */
+console.log(aoa * aob); /* Output: 20 */
 /* Division */
-console.log(aoa/aob);  /* Output: 1.25 */
+console.log(aoa / aob); /* Output: 1.25 */
 /* Remainder */
-console.log(aoa%aob);  /* Output: 1 */
+console.log(aoa % aob); /* Output: 1 */
 /* Powerof */
-console.log(aoa**aob); /* Output: 625 */
+console.log(aoa ** aob); /* Output: 625 */
 
 /* Type Casting */
 /* It is used to convert one data type to another data type */
 
 /* String to Number */
 let tca = 5;
-let tcb ='5';
-let stn = parseInt(tcb);  /* Another ways: "let stn = Number(tcb);","let stn = +(tcb);" */
+let tcb = "5";
+let stn =
+  parseInt(
+    tcb
+  ); /* Another ways: "let stn = Number(tcb);","let stn = +(tcb);" */
 /* parseFloat is used for decimal values */
-console.log(typeof(tca));  /* Output: number */
-console.log(typeof(tcb));  /* Output: string */
-console.log(typeof(stn));  /* Output: number */
+console.log(typeof tca); /* Output: number */
+console.log(typeof tcb); /* Output: string */
+console.log(typeof stn); /* Output: number */
 
 /* Logical Operators
    "&&" - AND
    "||" - OR */
-   let loa = 5;
-   let lob = 3;
-   let loc = 4;
-   let lod = 6;
-   console.log(loa>lob && loc<lod);  /* Output: true */
-   console.log(loa>lob && loc>lod);  /* Output: false */
-   /* It will check both condition is true */
-   console.log(loa>lob || loc<lod);  /* Output: true */
-   console.log(loa>lob || loc>lod);  /* Output: true */
-   /* It will check any one condition is true */
+let loa = 5;
+let lob = 3;
+let loc = 4;
+let lod = 6;
+console.log(loa > lob && loc < lod); /* Output: true */
+console.log(loa > lob && loc > lod); /* Output: false */
+/* It will check both condition is true */
+console.log(loa > lob || loc < lod); /* Output: true */
+console.log(loa > lob || loc > lod); /* Output: true */
+/* It will check any one condition is true */
 
 /* Truthy Values and Falsy Values */
 /* Truthy Values:
@@ -242,3 +245,186 @@ console.log(typeof(stn));  /* Output: number */
    Falsy Values:
       (0,'',0n,NaN,null)
  */
+
+/* FSD22A JS_4 */
+
+/* This is one of the ES6 feature  */
+let aa = 5;
+let bb = 5;
+console.log(`The addition of aa and bb is ${aa + bb}`);
+/* Output: The addition of aa and bb is 10 */
+
+/* To fixed function */
+let ffa = Math.random();
+console.log(ffa); /* Output: 0.655196400209521 */
+console.log(ffa.toFixed(2)); /* Output: 0.65 */
+
+/* Conditional Statements */
+/* If, Else, Elseif */
+
+let age = 18;
+if (age > 18) {
+  console.log(`You are ${age} years old. So, you're eligeable for voting`);
+} else if (age == 18) {
+  console.log(`You are ${age} years old. Are you registerd for voting?`);
+} else {
+  console.log(`You are ${age} years old. So, you're not eligeable for voting`);
+}
+/* Output:You are 18 years old. Are you registerd for voting? */
+
+/* Switch Statement */
+
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday");
+    break;
+  case "WedThursday":
+    console.log("Today is WedThursday");
+    break;
+  case "Thursday":
+    console.log("Today is Thursday");
+    break;
+  case "Friday":
+    console.log("Today is Friday");
+    break;
+  case "Saturday":
+    console.log("Today is Saturday");
+    break;
+  case "Sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Invalid Input");
+}
+/* Output: Today is Monday */
+
+/* FSD22A JS_6 */
+
+/* Functions */
+
+/* a) Declarative Function */
+
+function addition(numone, numtwo) {
+  let sum = numone + numtwo;
+  return sum;
+}
+console.log(addition(6, 5)); /* Output: 11 */
+
+/* b) Statement Function */
+
+const substraction = function (numone, numtwo) {
+  let sub = numone - numtwo;
+  return sub;
+};
+console.log(substraction(6, 5)); /* Output: 1 */
+
+/* c) Anonymous Function */
+/* It doesn't return anything */
+(function (numone, numtwo) {
+  let mul = numone * numtwo;
+  console.log(mul);
+})(6, 5); /* Output: 30 */
+
+/* d) Arrow Function (Implicit & Explicit) */
+
+/* Implicit */
+const division = (numone, numtwo) => numone / numtwo;
+console.log(division(100, 4)); /* Output: 25 */
+
+/* Explicit */
+/* In Explicit Arrow function, Return keyword is Mandatory */
+
+const modulas = (numone, numtwo) => {
+  let rem = numone % numtwo;
+  return rem;
+};
+console.log(modulas(25, 20)); /* Output: 5 */
+
+/* FSD22A JS_8 */
+
+/* Objects */
+
+const mydata = {
+  firstname: "Peter",
+  lastname: "Parker",
+  age: 24,
+  gender: "M",
+  isMarried: false,
+  college: "University of California",
+  cgpa: 8.6,
+  languages: ["english", "tamil"],
+  skills: {
+    frontend: "HTML,CSS,JavaScript",
+    backend: "Express.Js,Node.Js,MongoDb",
+  },
+};
+
+console.log(mydata); /* Output: 
+{ firstname: 'Peter',
+  lastname: 'Parker',
+  age: 24,
+  gender: 'M',
+  isMarried: false,
+  college: 'University of California',
+  cgpa: 8.6,
+  languages: [ 'english', 'tamil' ],
+  skills:
+   { frontend: 'HTML,CSS,JavaScript',
+     backend: 'Express.Js,Node.Js,MongoDb' } } */
+
+ console.log(mydata.age); /* Output: 24 */
+ console.log(mydata['cgpa']); /* Output: 8.6 */
+
+ /* Keys of Object */
+console.log(Object.keys(mydata));
+/* Output:
+[ 'firstname',
+  'lastname',
+  'age',
+  'gender',
+  'isMarried',
+  'college',
+  'cgpa',
+  'languages',
+  'skills' ] */
+console.log(Object.entries(mydata));
+/* Output:
+[ [ 'firstname', 'Peter' ],
+  [ 'lastname', 'Parker' ],
+  [ 'age', 24 ],
+  [ 'gender', 'M' ],
+  [ 'isMarried', false ],
+  [ 'college', 'University of California' ],
+  [ 'cgpa', 8.6 ],
+  [ 'languages', [ 'english', 'tamil' ] ],
+  [ 'skills',
+    { frontend: 'HTML,CSS,JavaScript',
+      backend: 'Express.Js,Node.Js,MongoDb' } ] ] */
+console.log(Object.values(mydata));
+/* Output:
+[ 'Peter',
+  'Parker',
+  24,
+  'M',
+  false,
+  'University of California',
+  8.6,
+  [ 'english', 'tamil' ],
+  { frontend: 'HTML,CSS,JavaScript',
+    backend: 'Express.Js,Node.Js,MongoDb' } ] */
+
+/* "this" Keyword */
+
+const thiseg ={
+      firstname: 'Peter',
+      lastname: 'Parker',
+      fullname: function(){
+            return (this.firstname+' '+this.lastname);
+      }
+}
+console.log(thiseg.fullname());  /* Output: Peter Parker */
